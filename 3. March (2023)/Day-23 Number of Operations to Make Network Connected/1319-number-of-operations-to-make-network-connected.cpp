a697665560cdace23eddf714ec11061915bc7a46
xavierbeast68 <sdby101@gmail.com>
2023-03-28 01:41:23 +0530

    updated

---
+++ 3. March (2023)/Day-23 Number of Operations to Make Network Connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,56 @@
+//* https://leetcode.com/problems/number-of-operations-to-make-network-connected/
+//* https://leetcode.com/problems/number-of-operations-to-make-network-connected/editorial/
+//* https://www.youtube.com/watch?v=T9KWTUolI04&ab_channel=DeepCodes
+
+class Solution
+{
+public:
+    void bfs(int node, vector<vector<int>> &adj, vector<bool> &visit)
+    {
+        queue<int> q;
+        q.push(node);
+        visit[node] = true;
+
+        while (!q.empty())
+        {
+            node = q.front();
+            q.pop();
+            for (int neighbor : adj[node])
+            {
+                if (!visit[neighbor])
+                {
+                    visit[neighbor] = true;
+                    q.push(neighbor);
+                }
+            }
+        }
+    }
+
+    int makeConnected(int n, vector<vector<int>> &connections)
+    {
+        if (connections.size() < n - 1)
+        {
+            return -1;
+        }
+
+        vector<vector<int>> adj(n);
+        for (auto &connection : connections)
+        {
+            adj[connection[0]].push_back(connection[1]);
+            adj[connection[1]].push_back(connection[0]);
+        }
+
+        int numberOfConnectedComponents = 0;
+        vector<bool> visit(n);
+        for (int i = 0; i < n; i++)
+        {
+            if (!visit[i])
+            {
+                numberOfConnectedComponents++;
+                bfs(i, adj, visit);
+            }
+        }
+
+        return numberOfConnectedComponents - 1;
+    }
+};
---
+++ 3. March (2023)/Day-25 Count Unreachable Pairs of Nodes in an Undirected Graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
@@ -0,0 +1,52 @@
+//* https://leetcode.com/problems/count-unreachable-pairs-of-nodes-in-an-undirected-graph/
+//* https://www.youtube.com/watch?v=dk5FLiiLcQA&ab_channel=CodeFreaks
+
+class Solution
+{
+public:
+    vector<int> vis;
+
+    int dfs(vector<vector<int>> &g, int i)
+    {
+
+        vis[i] = 1;
+        int ans = 1;
+        for (auto &j : g[i])
+        {
+            if (!vis[j])
+            {
+                ans += dfs(g, j);
+            }
+        }
+        return ans;
+    }
+
+    long long countPairs(int n, vector<vector<int>> &edges)
+    {
+
+        vector<vector<int>> g(n);
+        vis = vector<int>(n, 0);
+        for (auto &i : edges)
+        {
+            g[i[0]].push_back(i[1]);
+            g[i[1]].push_back(i[0]);
+        }
+        vector<int> clsize;
+        for (int i = 0; i < n; i++)
+        {
+            if (!vis[i])
+            {
+                int x = dfs(g, i);
+                clsize.push_back(x);
+            }
+        }
+        long long ans = 0;
+        long long t = 0;
+        for (int i = 0; i < clsize.size(); i++)
+        {
+            t += clsize[i];
+            ans += (n - t) * clsize[i];
+        }
+        return ans;
+    }
+};
